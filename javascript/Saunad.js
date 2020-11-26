f13e1cd23b1c079b22ad304b85f7514a43c36a08
erikpyaslane <73594730+erikpyaslane@users.noreply.github.com>
2020-11-26 14:48:34 +0200

    Add files via upload

---
+++ javascript/Saunad.js
@@ -0,0 +1,16 @@
+/* JavaSript oli võetud Youtube'i videost: https://www.youtube.com/watch?v=v186gRZYyo4&t=180s*/
+function readMore() {
+  var dots = document.getElementById("dots");
+  var more = document.getElementById("more");
+  var bttn = document.getElementById("bttn");
+  
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    bttn.innerHTML = "Loe lahemalt";
+    more.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      bttn.innerHTML = "Peida";
+      more.style.display = "inline";
+    }
+  }
